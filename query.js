f17d7c934ef1d1f743f6b8ca7dd17baffdc1f4c1
Deepali Singh <deepalisingh3233@gmail.com>
2021-10-14 20:10:30 +0530

    Solve problem

--- query.js
+++ query.js
@@ -4,7 +4,7 @@ let date = new Date;
 console.log("Name - " + name);
 console.log("Query - " + query);
 console.log("Current Time -" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-if (date >= 9 && date <= 9) {
+if (date <= 9 && date >= 12) {
     let queryId = Math.floor(Math.random() * 1000000);
     console.log("Query ID -" + queryId);
 }
